1e705b187f08de60ff743d1d394560a0d875f49c
Kaleb Garrett <kalebagarrett@gmail.com>
2023-10-12 16:55:17 -0400

    Fixed Swagger UI

--- MyGarden.API/Program.cs
+++ MyGarden.API/Program.cs
@@ -12,19 +12,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connectionString = Environment.GetEnvironmentVariable("AzureDbConnect");
+//var connectionString = Environment.GetEnvironmentVariable("AzureDbConnect");
 
 builder.Services.AddDbContext<GardenDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDbConnect")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = String.Empty;
+});
 
 app.UseHttpsRedirection();
 
